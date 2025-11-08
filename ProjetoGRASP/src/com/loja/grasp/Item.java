8af49c240f7643971a8ce3ce29d242a7ce41865e
gabriel alberto duarte de andrade <166182008+curupiracuzim@users.noreply.github.com>
2025-11-08 11:09:04 -0300

    Initial commit: Implementação dos padrões GRASP

---
+++ ProjetoGRASP/src/com/loja/grasp/Item.java
@@ -0,0 +1,23 @@
+package com.loja.grasp;
+
+public class Item {
+    private Produto produto;
+    private int quantidade;
+
+    public Item(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public double getSubtotal() {
+        return produto.getPreco() * quantidade;
+    }
+}
